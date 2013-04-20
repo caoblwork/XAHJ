408ffb740b28a689b057b5f56622374c99b4394e
caobl <qiliannujian_2006@126.com>
2013-04-21 02:09:31 +0800

    执法任务相关

---
+++ source/EMES/src/com/hangu/emes/dao/SupervisoryTaskDAO.java
@@ -0,0 +1,8 @@
+package com.hangu.emes.dao;
+
+import com.hangu.emes.entity.SupervisoryTask;
+import com.hangu.framework.dao.BaseDAO;
+
+public interface SupervisoryTaskDAO extends BaseDAO<SupervisoryTask, String> {
+
+}
